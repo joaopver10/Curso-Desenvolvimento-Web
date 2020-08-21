9f9f50d9d281f5f7d986af999f7be6e9e70fe01c
joaopver <joaovitorino@unigranrio.br>
2020-08-20 21:09:20 -0300

    Update script.js

---
+++ Exercicios-web( Javascript )/aula 01/script.js
@@ -0,0 +1,16 @@
+ /* function end(){
+    var endereco = {
+        rua: "Rua dos pinheiros",
+        numero: 1293,
+        bairro: "Centro",
+        cidade: "São Paulo",
+        uf: "SP"
+       };
+       console.log(`O usúario mora em ${endereco.cidade}/${endereco.uf}, no bairro ${endereco.bairro}, na rua ${endereco.rua} com nº ${endereco.numero} `)
+
+       return endereco     
+ }
+
+ end()
+*/
+
