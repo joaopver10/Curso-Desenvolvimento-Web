cbc80c8ea3e2d42e3bdf7f4063866f99a3b519e0
João Pedro <55984774+joaopver10@users.noreply.github.com>
2020-08-21 01:02:28 -0300

    Update script.js

--- Exercicios-web( Javascript )/aula 01/script.js
+++ Exercicios-web( Javascript )/aula 01/script.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 // Gerar valor padrão na função (caso precise criar variaveis com valores padrões esse é o melhor metodo)
 function soma1(a, b, c){
     a = a || 1
@@ -15,21 +14,4 @@ console.log(soma1(2,3,4))
 function soma2(a = 1, b = 1, c = 1){
     return a + b + c
 }
-=======
- /* function end(){
-    var endereco = {
-        rua: "Rua dos pinheiros",
-        numero: 1293,
-        bairro: "Centro",
-        cidade: "São Paulo",
-        uf: "SP"
-       };
-       console.log(`O usúario mora em ${endereco.cidade}/${endereco.uf}, no bairro ${endereco.bairro}, na rua ${endereco.rua} com nº ${endereco.numero} `)
-
-       return endereco     
- }
-
- end()
-*/
-
->>>>>>> 9f9f50d9d281f5f7d986af999f7be6e9e70fe01c
+ 
