16d88463b7eee319b769b6e2d1933e5a58513b40
joaopver <joaovitorino@unigranrio.br>
2020-08-25 21:20:58 -0300

    Update script.js

--- Exercicios-web( Javascript )/aula 08/script.js
+++ Exercicios-web( Javascript )/aula 08/script.js
@@ -9,3 +9,19 @@ const produto = {
     getPreco
 }
 console.log(produto.getPreco())
+
+const carro = { preco: 4500, desc: 0.15}
+
+console.log(getPreco.call(carro))
+console.log(getPreco.apply(carro))
+
+console.log(getPreco.call(carro, 0.17, "$"))
+console.log(getPreco.apply(carro, [0.17, "$"]))
+
+const produto2 = {
+    nome: "papel",
+    preco: 5.50,
+    desc: 0.5
+}
+
+console.log(getPreco.call(produto2))
