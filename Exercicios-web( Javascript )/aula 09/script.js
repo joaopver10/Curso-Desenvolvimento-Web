3bcfd7a42d9c8fff3cc36c1b556de447007da735
João Pedro <55984774+joaopver10@users.noreply.github.com>
2020-08-28 19:33:50 -0300

    Create script.js

---
+++ Exercicios-web( Javascript )/aula 09/script.js
@@ -0,0 +1,23 @@
+/* 
+function mostraValor(x, y){
+    console.log(`Soma de ${x} + ${y} é de: ${x + y}`)
+    console.log(`A subtração de ${x} - ${y} é de: ${x - y}`)
+    console.log(`A divisão entre ${x} / ${y} é de: ${x / y}`)
+    console.log(`A multiplicação entre ${x} x ${y} é de: ${x * y}`)
+}
+
+mostraValor(4, 2)
+
+*/
+
+function compara (lado1, lado2, lado3){
+
+    if(lado1 == lado2 && lado2 == lado3){
+        return "Equilátero"
+    }else if(){
+        
+    }
+
+}
+
+console.log(compara(2, 2, 2))
